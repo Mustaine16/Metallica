a437808bad806c4e9b104d10f11db7b85726fedb
Eze <ezequieldominguezcp11@gmail.com>
2019-06-25 12:59:09 -0300

    Ahora si, es 100% responsive y agregue la navbar en mobile

---
+++ js.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var glide_modular_esm_1 = require("@glidejs/glide/dist/glide.modular.esm");
+new glide_modular_esm_1["default"]('.glide').mount({ Images: glide_modular_esm_1.Images });
---
+++ js.ts
@@ -0,0 +1,3 @@
+import Glide, { Images } from '@glidejs/glide/dist/glide.modular.esm'
+
+new Glide('.glide').mount({ Images })
